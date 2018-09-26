d6c12314494f18cc89784dbac428214f2ea957b7
Sudeepthi Peteti <sudeepthi.p@techaspect.com>
2018-09-26 17:53:43 +0530

    Added Source Code for deleteduplicatevaluenodesfromsortedlinkedlist

---
+++ Day_5/deleteduplicatevaluenodesfromsortedlinkedlist.js
@@ -0,0 +1,34 @@
+// Complete the removeDuplicates function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function removeDuplicates(head) {
+        var temp=head;
+        var node=null;
+        while(temp.next!=null){
+                
+                node=temp.next;
+                
+                if(temp.data==node.data){
+                    if(temp==head){
+                        head=temp.next;
+                        temp=head;
+                    }
+                    else{
+                    temp.next=node.next;
+                    }
+                }
+                else{
+                    temp=temp.next;
+                }
+            }
+        return head;
+
+}
