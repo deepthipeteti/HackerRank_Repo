089f7d3daeebd2e7f6461147b80d706c46556792
Sudeepthi Peteti <sudeepthi.p@techaspect.com>
2018-09-26 16:42:03 +0530

    Added Source Code for delete node from linked list

---
+++ Day_5/deletenodefromlinkedlist.js
@@ -0,0 +1,28 @@
+// Complete the deleteNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function deleteNode(head, position) {
+        var node,temp;
+        temp=head;
+        if(position==0){
+            head=head.next;    
+        }
+        else{
+        for(var i=0;i<position-1;i++){
+            temp=temp.next;
+        }
+        node=temp.next;
+        temp.next=node.next;
+        }
+       
+        return head;
+
+}
