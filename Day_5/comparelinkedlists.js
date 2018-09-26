0d225956d10f66db25d0ef1f568017c50ad7128c
Sudeepthi Peteti <sudeepthi.p@techaspect.com>
2018-09-26 16:52:57 +0530

    Added Source Code for compare two linkedlists

---
+++ Day_5/comparelinkedlists.js
@@ -0,0 +1,26 @@
+// Complete the CompareLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function CompareLists(head1, head2) {
+    var temp1=head1;
+    var temp2=head2;
+    var status=true;
+    while((temp1!=null)&&(temp2!=null)){
+    if(temp1.data!=temp2.data)
+     status=false;
+    temp1=temp1.next;
+    temp2=temp2.next;
+    }
+    if(!(temp1==null)&&(temp2==null))
+        status=false;
+return status;
+
+}
