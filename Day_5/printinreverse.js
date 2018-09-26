b7e0e81affe6c936495fbde160a1e0f9bf18da9a
Sudeepthi Peteti <sudeepthi.p@techaspect.com>
2018-09-26 16:46:06 +0530

    Added Source Code for print in reverse of linkedlist

---
+++ Day_5/printinreverse.js
@@ -0,0 +1,24 @@
+// Complete the reversePrint function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function reversePrint(head) {
+             var temp=head;
+            var arr=new Array(1000);
+            var i=0;
+            while(temp!=null){
+                arr[i]=temp.data;
+                i++;
+                temp=temp.next;
+            }
+            for(var j=i-1;j>=0;j--)
+                console.log(arr[j]);
+
+}
